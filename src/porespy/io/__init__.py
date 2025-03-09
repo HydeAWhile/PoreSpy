4be373a6fc5eb7316b96a2106b325dfc15a6cc95
jgostick <jgostick@gmail.com>
2025-03-09 14:56:21 -0400

    removing maximal ball wrapper

--- src/porespy/io/__init__.py
+++ src/porespy/io/__init__.py
@@ -16,7 +16,6 @@ Collection of functions for importing and exporting images
     io.to_stl
     io.to_paraview
     io.open_paraview
-    io.spheres_to_comsol
 
 """
 
@@ -27,5 +26,4 @@ from ._funcs import openpnm_to_im
 from ._funcs import to_stl
 from ._funcs import to_paraview
 from ._funcs import open_paraview
-from ._funcs import spheres_to_comsol
 from ._unzipper import *
