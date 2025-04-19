b89675f515f0ce757b8d9dcd83f328f9603b87e2
jgostick <jgostick@gmail.com>
2025-04-18 22:54:56 -0400

    moving dilation and erode to filters

--- src/porespy/filters/__init__.py
+++ src/porespy/filters/__init__.py
@@ -17,29 +17,36 @@ image, but with altered values.
     filters.apply_chords
     filters.apply_chords_3D
     filters.apply_padded
+    filters.capillary_transform
     filters.chunked_func
+    filters.dilate
     filters.distance_transform_lin
+    filters.erode
     filters.fftmorphology
     filters.fill_closed_pores
+    filters.fill_trapped_voxels
+    filters.find_closed_pores
     filters.find_disconnected_voxels
     filters.find_dt_artifacts
+    filters.find_invalid_pores
     filters.find_peaks
+    filters.find_surface_pores
     filters.find_trapped_regions
     filters.flood
     filters.flood_func
     filters.hold_peaks
-    filters.ibip
-    filters.ibip_gpu
     filters.local_thickness
     filters.nl_means_layered
     filters.nphase_border
     filters.pc_to_satn
+    filters.pc_to_seq
     filters.porosimetry
     filters.prune_branches
     filters.reduce_peaks
     filters.region_size
     filters.satn_to_seq
     filters.seq_to_satn
+    filters.size_to_pc
     filters.size_to_satn
     filters.size_to_seq
     filters.snow_partitioning
@@ -51,6 +58,7 @@ image, but with altered values.
     filters.trim_nearby_peaks
     filters.trim_nonpercolating_paths
     filters.trim_saddle_points
+    filters.trim_saddle_points_legacy
     filters.trim_small_clusters
 
 """
@@ -63,3 +71,4 @@ from ._size_seq_satn import *
 from ._snows import *
 from ._transforms import *
 from ._invasion import *
+from ._morphology import *
---
+++ src/porespy/filters/_morphology.py
@@ -0,0 +1,67 @@
+import numpy as np
+from scipy.signal import fftconvolve
+from porespy.tools import (
+    ps_round,
+    get_edt
+)
+
+
+edt = get_edt()
+
+
+__all__ = [
+    'erode',
+    'dilate',
+]
+
+
+def erode(im, r, dt=None, method='dt', smooth=True):
+    from porespy import settings
+    if dt is None:
+        dt = edt(im, parallel=settings.ncores)
+    if method == 'dt':
+        ero = dt >= r if smooth else dt > r
+    elif method.startswith('conv'):
+        se = ps_round(r=r, ndim=im.ndim, smooth=smooth)
+        ero = ~(fftconvolve(~im, se, mode='same') > 0.1)
+    return ero
+
+
+def dilate(im, r, method='dt', smooth=True):
+    from porespy import settings
+    im = im == 1
+    if method == 'dt':
+        dt = edt(~im, parallel=settings.ncores)
+        dil = dt < r if smooth else dt <= r
+        dil += im
+    elif method.startswith('conv'):
+        se = ps_round(r=r, ndim=im.ndim, smooth=smooth)
+        dil = fftconvolve(im, se, mode='same') > 0.1
+    return dil
+
+
+if __name__ == "__main__":
+    import porespy as ps
+    import matplotlib.pyplot as plt
+
+    im = ps.generators.blobs([200, 200], porosity=0.6, seed=5)
+
+    ero1 = erode(im, 5, method='dt').astype(int)
+    ero1[~im] = -1
+
+    ero2 = erode(im, 5, method='conv').astype(int)
+    ero2[~im] = -1
+
+    fig, ax = plt.subplots(2, 2)
+    ax[0][0].imshow(ero1)
+    ax[0][1].imshow(ero2)
+
+    ero1 = erode(im, 10, method='dt').astype(int)
+    dil1 = dilate(ero1, 10, method='dt').astype(int)
+    dil1[~im] = -1
+
+    dil2 = dilate(ero1, 10, method='conv').astype(int)
+    dil2[~im] = -1
+
+    ax[1][0].imshow(dil1)
+    ax[1][1].imshow(dil2)
