3029f45415fce0427577a03ae1688b7304ffbc01
Amin Sadeghi <amin.sadeghi@live.com>
2025-05-16 14:55:21 -0700

    feat: streamline build process to just work using `hatch build` or `uv build`

--- hatch_build.py
+++ hatch_build.py
@@ -13,14 +13,21 @@ class CustomBuildHook(BuildHookInterface):
     def initialize(self, version, build_data):
         build_data['infer_tag'] = True  # Ensure platform-specific wheel tag
 
-        # Determine platform and choose the right build script
-        if os.name == "nt":
-            script = "./scripts/build.bat"
-        else:
-            script = "./scripts/build.sh"
+        # # Determine platform and choose the right build script
+        # if os.name == "nt":
+        #     script = "./scripts/build.bat"
+        # else:
+        #     script = "./scripts/build.sh"
 
-        print(f"[hook] Running build script: {script}")
-        result = subprocess.run(script, shell=True)
+        # print(f"[hook] Running build script: {script}")
+        # result = subprocess.run(script, shell=True)
+
+        result = subprocess.run(['meson', 'setup', 'builddir'], check=True)
+
+        if result.returncode != 0:
+            sys.exit(f"[hook] Build script failed with exit code {result.returncode}")
+
+        result = subprocess.run(['meson', 'compile', '-C', 'builddir'], check=True)
 
         if result.returncode != 0:
             sys.exit(f"[hook] Build script failed with exit code {result.returncode}")
